dc87aaf204602f6809565fb7016f495ca1033956
Gustavo Santos <gsantosgonzalez@hotmail.com>
2019-02-16 16:15:09 -0600

    Fix test host

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ import '../index.js';
 
 describe('Example node server', () => {
     it('Should return 200', done => {
-        http.get('http://192.168.0.31:1337', res => {
+        http.get('http://localhost:1337', res => {
             assert.equal(200, res.statusCode);
             done();
         });
